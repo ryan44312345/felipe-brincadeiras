4704b20ca9566ce40a26a60db684b33fc312b48a
Ryan <ryanlucasstos@gmail,com>
2026-08-02 01:38:46 -0300

    sem requisição

--- felipe-brincadeiras/src/services/HomeService.ts
+++ felipe-brincadeiras/src/services/HomeService.ts
@@ -3,13 +3,18 @@ import { useState, useEffect } from "react";
 export function HomeService() {
     const [loading, setLoading] = useState(true);
     const [message, setMessage] = useState("");
-    
+
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const result = await fetch(`${import.meta.env.VITE_API_URL}`);
-                const text = await result.text();
-                setMessage(text);
+                const result = await fetch(import.meta.env.VITE_API_URL + "/hello", {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json"
+                    }
+                });
+                const json = await result.json();
+                setMessage(json.message);
             } catch (error) {
                 console.error("Failed to fetch data:", error);
             } finally {
