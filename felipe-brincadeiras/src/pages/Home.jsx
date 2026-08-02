2b503048568f8c5dde679754e57ba44d541bd527
Ryan <ryanlucasstos@gmail,com>
2026-08-02 01:48:55 -0300

    tentativa 3

--- felipe-brincadeiras/src/pages/Home.jsx
+++ felipe-brincadeiras/src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import { HomeService } from "../services/HomeService"
 
 export function Home() {
-    const { loading, message } = HomeService();
+    const { message } = HomeService();
 
     return (
-        <h1>{message}</h1>
+        <>
+            <h1>{message}</h1>
+            <h1>{loading ? "loading..." : "Carregou"}</h1>
+        </>
     )
 }
