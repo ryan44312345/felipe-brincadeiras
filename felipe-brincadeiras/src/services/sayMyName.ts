a2f0a348566b172dee008a843229494fc35053ea
Ryan <ryanlucasstos@gmail,com>
2026-08-02 15:50:18 -0300

    saymyname2

--- felipe-brincadeiras/src/services/sayMyName.ts
+++ felipe-brincadeiras/src/services/sayMyName.ts
@@ -3,9 +3,5 @@
 export async function fetchHelloName(name: string): Promise<string> {
     const api_url = import.meta.env.VITE_API_URL;
 
-    if (!api_url.endsWith("/")) {
-        throw new Error("Deu ruim");
-    }
-
-    return api_url + name;
+    return api_url + "/" + name;
 }
