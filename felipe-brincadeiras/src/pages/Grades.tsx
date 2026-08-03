8485933e416c9ce2ff6a400d2fc1a4a4d43a2d40
Ryan <ryanlucasstos@gmail.com>
2026-08-03 13:08:18 -0300

    tentativa do gemido 1

--- felipe-brincadeiras/src/pages/Grades.tsx
+++ felipe-brincadeiras/src/pages/Grades.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { usePostGrades } from "../hooks/usePostGrade";
 import patrickGeniusBg from "../assets/patrick-genius.jpg";
 import audioFile from "../assets/Video Project.m4a";
